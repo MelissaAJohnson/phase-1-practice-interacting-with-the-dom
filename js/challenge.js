0e479e13374b4a4729584607fe9031eabe993e8d
Melissa Johnson <missyj2007@gmail.com>
2023-01-04 20:57:07 -0600

    Implemented plus and minus buttons

--- js/challenge.js
+++ js/challenge.js
@@ -1,15 +1,42 @@
-// variable to store intervalID
+// initalize variables
 let nIntervId;
+let counter = 0;
+const node = document.getElementById('counter');
+const pauseButton = document.getElementById('pause');
+const incrementButton = document.getElementById('plus');
+const decrementButton = document.getElementById('minus');
 
 // Set the timer increment every second once page is loaded
 document.addEventListener('DOMContentLoaded', () => {
-    const node = document.getElementById('counter');
-    let counter = 0;
     if (!nIntervId) {
         nIntervId = setInterval(() => {
             counter ++;
-            console.log(counter);
             node.innerHTML = counter;
+
+            pauseButton.addEventListener("click", pauseCounter);
+            
         }, 1000)
     }
+
+    // Pause counter
+    function pauseCounter() {
+        clearInterval(nIntervId);
+        pauseButton.innerHTML = "resume";
+
+        incrementButton.addEventListener("click", function(){incrementCounter(counter)});
+
+        decrementButton.addEventListener("click", function(){decrementCounter(counter)});
+    }
 })
+
+// Increment counter
+function incrementCounter(start) {
+    counter = start + 1;
+    node.innerHTML = counter;
+}
+
+// Decrement counter
+function decrementCounter(start) {
+    counter = start - 1;
+    node.innerHTML = counter;
+}
